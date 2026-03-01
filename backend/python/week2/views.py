06550b453384609fc15b3637b8fbb9f8e8a42e28
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2026-03-01 19:32:15 +0530

    refactored and pagination added

--- backend/python/week2/views.py
+++ backend/python/week2/views.py
@@ -1,36 +1,42 @@
 from rest_framework import viewsets
 from .models import Product, Category, Brand
 from .serializers import ProductSerializer, CategorySerializer, BrandSerializer
-from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework.status import HTTP_401_UNAUTHORIZED
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
+
+class IsAdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return bool(request.user and request.user.is_staff)
+    
+class CustomPagination(PageNumberPagination):
+    page_size = 2
 
 class ProductViewSet(viewsets.ModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def create(self, request, *args, **kwargs):
-        if(request.user and request.user.is_staff):
-            return super().create(request, *args, **kwargs)
-        else:
-            return Response(status=HTTP_401_UNAUTHORIZED, data = b'Only Admin can add products')
-    
-    def update(self, request, *args, **kwargs):
-        if(request.user and request.user.is_staff):
-            return super().update(request, *args, **kwargs)
-        else:
-            return Response(status=HTTP_401_UNAUTHORIZED, data = b'Only Admin can update products')
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = CustomPagination
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = CustomPagination
 
 
 class BrandViewSet(viewsets.ModelViewSet):
 
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = CustomPagination
